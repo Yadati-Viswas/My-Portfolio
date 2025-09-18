eed95ba44830cac8c84ee5d949ca2aa3721c3ca6
Yadati-Viswas <yadatisaiviswas3@gmail.com>
2025-09-17 23:28:06 -0400

    My Portfolio.

---
+++ src/data/ProjectData.js
@@ -0,0 +1,68 @@
+// --------------------------------------- Project List
+import EventEase from "../Assets/Event-Ease.png";
+import OmniQuiz from "../Assets/OmniQuiz.png";
+import Portfolio from "../Assets/portfolio.png";
+export const ProjectList = [
+  {
+    img: EventEase,
+    title: "Event Ease",
+    description: "Event Ease is a event management system designed to connect event organizers with attendees. It offers a user-friendly platform for creating, managing, and discovering events, making it easier for organizers to reach their audience and for attendees to find events that interest them.",
+    tech_stack: "Html, Tailwind Css, Reactjs, Nodejs, Expressjs, MongoDB",
+    github_url: "https://github.com/Yadati-Viswas/EventEase",
+    demo_url: "https://event-ease-frontend-v1.vercel.app",
+  },
+  {
+    img: OmniQuiz,
+    title: "OmniQuiz",
+    description:
+      "Teachers can craft engaging quizzes for their classes, while everyone else can jump in, test their knowledge, and climb the leaderboards!",
+    tech_stack: "Html, Tailwind Css, ReactJs, Spring Boot, PostgreSQL",
+    github_url: "https://github.com/Yadati-Viswas/Quiz-App",
+    demo_url: "https://github.com/Yadati-Viswas/Quiz-App",
+  },
+  {
+    img: Portfolio,
+    title: "Portfolio",
+    description: "Ofcourse i made my own personal portfolio website.",
+    tech_stack: "Reactjs, Css",
+    github_url: "https://your_project_github_link",
+    demo_url: "https://your_project_demo_link/",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/css3.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/javascript.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/bootstrap.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/sass.svg",
+    name: "Scss",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
+    name: "Responsive",
+  },
+];
